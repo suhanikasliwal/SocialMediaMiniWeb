b5ef7b228b2212d8dfa4aa479ab41ac0a9c2f2cb
suhanikasliwal <suhanikasliwal32@gmail.com>
2026-02-03 19:51:35 +0530

    models

---
+++ backend/models/messages.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    chatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Use singular name & default export
+export default mongoose.model("Message", messageSchema);
